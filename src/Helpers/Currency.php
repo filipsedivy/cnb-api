04ea48b00d7874ffa24a5af810356b32b39eca7a
Filip Šedivý <mail@filipsedivy.cz>
2019-04-27 12:46:24 +0200

    Add currency helper

---
+++ src/Helpers/Currency.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CnbApi\Helpers;
+
+class Currency
+{
+    const EURO = 'EUR',
+        DOLAR = 'USD',
+        LIBRA = 'GBP',
+        RUPIE = 'IDR',
+        JEN = 'JPY',
+        LIRA = 'TRY',
+        RAND = 'ZAR',
+        ZLOTY = 'PLN',
+        RUBL = 'RUB',
+        FORINT = 'HUF';
+}
