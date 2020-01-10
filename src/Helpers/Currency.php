e294998334d418787c98ed1b2cdc40ce9f6a11a2
Filip Šedivý <mail@filipsedivy.cz>
2020-01-10 02:41:59 +0100

    Add all currencies

--- src/Helpers/Currency.php
+++ src/Helpers/Currency.php
@@ -1,19 +1,40 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace CnbApi\Helpers;
 
 class Currency
 {
-    const EURO = 'EUR',
-        DOLAR = 'USD',
-        LIBRA = 'GBP',
-        RUPIE = 'IDR',
+    public const AUSTRALSKY_DOLAR = 'AUD',
+        REAL = 'BRL',
+        LEV = 'BGN',
+        ZEN_MIN_PI = 'CNY',
+        DANSKA_KORUNA = 'DKK',
+        EURO = 'EUR',
+        FILIPINSKE_PESO = 'PHP',
+        HONGKONGSKY_DOLAR = 'HKD',
+        KUNA = 'HRK',
+        INDICKA_RUPIE = 'INR',
+        INDONESKA_RUPIE = 'IDR',
+        ISLANDSKA_KORUNA = 'ISK',
+        NOVY_SEKEL = 'ILS',
         JEN = 'JPY',
-        LIRA = 'TRY',
         RAND = 'ZAR',
+        KANADSKY_DOLAR = 'CAD',
+        WON = 'KRW',
+        FORINT = 'HUF',
+        RINGGIT = 'MYR',
+        MEXICKE_PESO = 'MXN',
+        ZPC = 'XDR',
+        NORSKA_KORUNA = 'NOK',
+        NOVOZELANDSKY_DOLAR = 'NZD',
         ZLOTY = 'PLN',
+        LEU = 'RON',
         RUBL = 'RUB',
-        FORINT = 'HUF';
+        SINGAPURSKY_DOLAR = 'SGD',
+        SVEDSKA_KORUNA = 'SEK',
+        FRANK = 'CHF',
+        BAHT = 'THB',
+        LIRA = 'TRY',
+        DOLAR = 'USD',
+        LIBRA = 'GBP';
 }
