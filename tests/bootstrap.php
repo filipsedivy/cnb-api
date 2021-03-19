758bb692c6ec8aed69b745fc77efdbfb4d386c6b
Filip Šedivý <mail@filipsedivy.cz>
2021-03-20 00:59:49 +0100

    Delete unused files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,8 @@
 <?php declare(strict_types=1);
 
-use Nette\Loaders;
-use Ninjify\Nunjuck\Environment;
-
-if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    echo 'Install Nette Tester using `composer update --dev`';
-    die(0);
-}
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
-Environment::setupTester();
-Environment::setupTimezone();
-Environment::setupVariables(__DIR__);
-Environment::setupRobotLoader(static function (Loaders\RobotLoader $robotLoader) {
-    $robotLoader->addDirectory(__DIR__ . '/engine');
-});
-
-define('DATA_DIR', __DIR__ . '/data');
+define(
+    'TEST_FILES_PATH',
+    __DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR,
+);
